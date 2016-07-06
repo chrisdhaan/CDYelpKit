c1a6fe98f2fb3f45d51cc8c654561c4837e6fb9a
Christopher de Haan <chrisdhaan@gmail.com>
2016-07-05 17:12:47 -0700

    chore(pod):update - Updated CDYelpKit in Example project

--- Example/Pods/Target Support Files/CDYelpKit/CDYelpKit-umbrella.h
+++ Example/Pods/Target Support Files/CDYelpKit/CDYelpKit-umbrella.h
@@ -1,21 +1,21 @@
 #import <UIKit/UIKit.h>
 
 #import "CDYelpAPIClient.h"
+#import "CDYelpKit.h"
 #import "CDYelpBusiness.h"
-#import "CDYelpBusinessResponse.h"
 #import "CDYelpDeal.h"
 #import "CDYelpDealOption.h"
 #import "CDYelpDetailedBusiness.h"
 #import "CDYelpError.h"
 #import "CDYelpGiftCertificate.h"
 #import "CDYelpGiftCertificateOption.h"
-#import "CDYelpKit.h"
 #import "CDYelpLocation.h"
 #import "CDYelpModels.h"
-#import "CDYelpResponses.h"
 #import "CDYelpReview.h"
-#import "CDYelpSearchResponse.h"
 #import "CDYelpUser.h"
+#import "CDYelpBusinessResponse.h"
+#import "CDYelpResponses.h"
+#import "CDYelpSearchResponse.h"
 
 FOUNDATION_EXPORT double CDYelpKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char CDYelpKitVersionString[];

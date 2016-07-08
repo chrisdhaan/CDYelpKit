dc572a6236c63007012b0321c2657f78d90451d5
Christopher de Haan <chrisdhaan@gmail.com>
2016-07-08 13:35:17 -0700

    fix(project):file structure - Fixed error with source_files definition in podspec - Updated CDYelpKit in Example project - Updated version number

---
+++ Example/Pods/Target Support Files/CDYelpKit/CDYelpKit-umbrella.h
@@ -0,0 +1,25 @@
+#import <UIKit/UIKit.h>
+
+#import "CDYelpAPIClient.h"
+#import "CDYelpKit.h"
+#import "CDYelpModels.h"
+#import "CDYelpResponses.h"
+#import "CDYelpQueryParams.h"
+#import "CDYelpRequestLocation.h"
+#import "CDYelpSortType.h"
+#import "CDYelpBusinessResponse.h"
+#import "CDYelpSearchResponse.h"
+#import "CDYelpBusiness.h"
+#import "CDYelpDeal.h"
+#import "CDYelpDealOption.h"
+#import "CDYelpDetailedBusiness.h"
+#import "CDYelpError.h"
+#import "CDYelpGiftCertificate.h"
+#import "CDYelpGiftCertificateOption.h"
+#import "CDYelpLocation.h"
+#import "CDYelpReview.h"
+#import "CDYelpUser.h"
+
+FOUNDATION_EXPORT double CDYelpKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char CDYelpKitVersionString[];
+
